21745ba5b6a5c036e6fe7290ac8cf2d263e5bcfa
Jay <jay.cn@outlook.com>
2017-09-23 15:21:24 +0800

    face detection with haar cascade
    scale factor:1.1, minfeature:3

---
+++ haarcascades/image_face_detection_haar.py
@@ -0,0 +1,23 @@
+ # -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 23 02:23:57 2017
+
+@author: Administrator
+"""
+
+import cv2
+import matplotlib.pyplot as plt
+img = cv2.imread('people.jpg')
+ 
+cascade_frontface = cv2.CascadeClassifier("haarcascade_profileface.xml")
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY )
+faces = cascade_frontface.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=3)
+for face in faces:
+    x,y,w,h = face
+    print(face)
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+cv2.imshow("img",img)
+img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)    
+plt.imshow(img)
+cv2.waitKey()
+cv2.destroyAllWindows()
